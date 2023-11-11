14f8439987e4e9b0992fc6431b87a3a2fa3b704e
mg-makari <marcel.giercarz@gmail.com>
2023-11-11 16:06:15 +0100

    Code reformat

--- monitor.py
+++ monitor.py
@@ -12,12 +12,12 @@ from discord_webhook import DiscordWebhook, DiscordEmbed
 requests.packages.urllib3.util.ssl_.DEFAULT_CIPHERS = 'ALL:@SECLEVEL=0'
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 HTTPConnection.default_socket_options = (
-    HTTPConnection.default_socket_options + [
-        (socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1),
-        (socket.SOL_TCP, socket.TCP_KEEPIDLE, 45),
-        (socket.SOL_TCP, socket.TCP_KEEPINTVL, 10),
-        (socket.SOL_TCP, socket.TCP_KEEPCNT, 6)
-    ]
+        HTTPConnection.default_socket_options + [
+    (socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1),
+    (socket.SOL_TCP, socket.TCP_KEEPIDLE, 45),
+    (socket.SOL_TCP, socket.TCP_KEEPINTVL, 10),
+    (socket.SOL_TCP, socket.TCP_KEEPCNT, 6)
+]
 )
 
 headers = {
@@ -29,16 +29,31 @@ headers = {
 }
 
 logging = logging.getLogger(__name__)
-coloredlogs.install(level='DEBUG', logger=logging, fmt="[%(asctime)s] %(message)s", milliseconds=True, field_styles={ 'hostname': {'color': 'magenta'}, 'levelname': {'bold': True, 'color': 'black'}, 'name': {'color': 'blue'}, 'programname': {'color': 'cyan'}, 'username': {'color': 'yellow'}})
+coloredlogs.install(level='DEBUG', logger=logging, fmt="[%(asctime)s] %(message)s", milliseconds=True,
+                    field_styles={'hostname': {'color': 'magenta'}, 'levelname': {'bold': True, 'color': 'black'},
+                                  'name': {'color': 'blue'}, 'programname': {'color': 'cyan'},
+                                  'username': {'color': 'yellow'}})
+
 
 class Config:
-    def __init__(self, config_file):
+    DEFAULT_CONFIG = {
+        "login": "",
+        "password": "",
+        "max_exam_time": 7,
+        "category": "B",
+        "word_id": "",
+        "discord_webhook_url": "",
+        "delay": 10,
+    }
+
+    def __init__(self, config_file: str):
+
         self.config_file = config_file
         self.email = None
         self.password = None
         self.webhook_url = None
         self.word_id = None
-        self.delay : int = 10
+        self.delay = 10
         self.category = "B"
         self.max_exam_time = 10
         self.load_config()
@@ -58,21 +73,23 @@ class Config:
         except FileNotFoundError:
             logging.error(f"Config file doesn't exist, creating...")
             with open(f"{self.config_file}", "w") as f:
-                json.dump({"login" : "","password" : "","max_exam_time" : 7,"category" : "B","word_id" : "","discord_webhook_url" : "","delay" : 10}, f, indent=4)
+                json.dump(Config.DEFAULT_CONFIG,
+                          f, indent=4)
             exit()
         except json.decoder.JSONDecodeError:
             logging.error("Wrong format in config.json, delete it and run the program again")
 
+
 class Monitor:
     def __init__(self) -> None:
         config = Config("config.json")
-        self.email : str = config.email
-        self.password : str = config.password
-        self.webhook_url : str = config.webhook_url
-        self.delay : int = config.delay
-        self.word_id : str = config.word_id
-        self.category : str = config.category
-        self.max_exam_time : int = config.max_exam_time
+        self.email: str = config.email
+        self.password: str = config.password
+        self.webhook_url: str = config.webhook_url
+        self.delay: int = config.delay
+        self.word_id: str = config.word_id
+        self.category: str = config.category
+        self.max_exam_time: int = config.max_exam_time
 
         self.auth_token = self.get_auth_token()
         self.scrap_dates()
@@ -82,54 +99,61 @@ class Monitor:
         retries = 0
 
         while retries < max_retries:
-            try:
-                with requests.Session() as s:
-                    logging.info("Logging in...")
-                    url = "https://info-car.pl/oauth2/login"
-                    try:
-                        r = s.get(url, headers=headers, verify=False, allow_redirects=False, timeout=30)
-                        soup = BeautifulSoup(r.text, 'lxml')
-                        csrfToken = soup.find('input', attrs={'name': '_csrf'})['value']
-                        cookies = s.cookies.get_dict()
-                    except Exception as e:
-                        logging.error(f"Error while scraping csrf_token: {e}")
-                        retries += 1
-                        time.sleep(5)
+            with requests.Session() as s:
+                logging.info("Logging in...")
+                url = "https://info-car.pl/oauth2/login"
+                try:
+                    r = s.get(url, headers=headers, verify=False, allow_redirects=False, timeout=30)
+                    soup = BeautifulSoup(r.text, 'lxml')
+                    csrfToken = soup.find('input', attrs={'name': '_csrf'})['value']
+                    cookies = s.cookies.get_dict()
+                except Exception as e:
+                    logging.error(f"Error while scraping csrf_token: {e}")
+                    retries += 1
+                    time.sleep(5)
 
-                    data = [
-                        ('username', self.email),
-                        ('_csrf', csrfToken),
-                        ('password', self.password),
-                        ('_csrf', csrfToken),
-                    ]
+                data = [
+                    ('username', self.email),
+                    ('_csrf', csrfToken),
+                    ('password', self.password),
+                    ('_csrf', csrfToken),
+                ]
 
-                    try:
-                        r = s.post(url, headers=headers, data=data, cookies=cookies, allow_redirects=True, verify=False, timeout=30)
-                        cookies = s.cookies.get_dict()
-                    except Exception as e:
-                        logging.error(f"Send form error: {e}")
-                        retries += 1
-                        time.sleep(5)
+                try:
+                    r = s.post(url,
+                               headers=headers,
+                               data=data,
+                               cookies=cookies,
+                               allow_redirects=True,
+                               verify=False,
+                               timeout=30)
+                    cookies = s.cookies.get_dict()
+                except Exception as e:
+                    logging.error(f"Send form error: {e}")
+                    retries += 1
+                    time.sleep(5)
 
-                    try:
-                        r = s.get(
-                            'https://info-car.pl/oauth2/authorize?response_type=id_token%20token&client_id=client&redirect_uri=https%3A%2F%2Finfo-car.pl%2Fnew%2Fassets%2Frefresh.html&scope=openid%20profile%20email%20resource.read&prompt=none',
-                            headers=headers, cookies=cookies, allow_redirects=False, verify=False, timeout=30)
-                        if r.headers["Location"].replace("=", " ").replace("&", " ").split()[1] == "login_required":
-                            logging.error("Account doesn't exist")
-                            exit()
-                        auth_token = "Bearer " + r.headers["Location"].replace("=", " ").replace("&", " ").split()[1]
-                        logging.debug("Successful logged in")
-                        print(auth_token)
-                        return auth_token
-                    except Exception as e:
-                        logging.error(f"Error while getting auth token: {e}")
-            except Exception as e:
-                logging.error(f"Error while getting auth token: {e}")
-                retries += 1
-                time.sleep(5)
+                try:
+                    r = s.get(
+                        'https://info-car.pl/oauth2/authorize?response_type=id_token%20token&client_id=client&redirect_uri=https%3A%2F%2Finfo-car.pl%2Fnew%2Fassets%2Frefresh.html&scope=openid%20profile%20email%20resource.read&prompt=none',
+                        headers=headers,
+                        cookies=cookies,
+                        allow_redirects=False,
+                        verify=False,
+                        timeout=30)
+                    if r.headers["Location"].replace("=", " ").replace("&", " ").split()[1] == "login_required":
+                        logging.error("Account doesn't exist")
+                        exit()
+                    auth_token = "Bearer " + r.headers["Location"].replace("=", " ").replace("&", " ").split()[1]
+                    logging.debug("Successful logged in")
+                    return auth_token
+                except Exception as e:
+                    logging.error(f"Error while getting auth token: {e}")
+                    retries += 1
+                    time.sleep(5)
 
-        logging.error("The login retries limit exceeded. Check your credentials.")
+    logging.error("The login retries limit exceeded. Check your credentials.")
+
 
     def scrap_dates(self) -> None:
         max_retries = 3
@@ -184,25 +208,11 @@ class Monitor:
                             exam_day = exam["day"]
                             exam_time = exam["scheduledHours"][0]["time"]
 
-                            if datetime.strptime(exam_day + " " + exam_time, "%Y-%m-%d %H:%M:%S").timestamp() < max_date and datetime.strptime(exam_day + " " + exam_time, "%Y-%m-%d %H:%M:%S").timestamp() != last_date:
+                            if datetime.strptime(exam_day + " " + exam_time,
+                                                 "%Y-%m-%d %H:%M:%S").timestamp() < max_date and datetime.strptime(
+                                    exam_day + " " + exam_time, "%Y-%m-%d %H:%M:%S").timestamp() != last_date:
                                 logging.debug(f"Wolny termin dnia: {exam_day} na godzine: {exam_time}")
-
-                                webhook = DiscordWebhook(
-                                    url=self.webhook_url,
-                                    content="@everyone")
-                                webhook.execute()
-
-                                webhook = DiscordWebhook(
-                                    url=self.webhook_url)
-                                embed = DiscordEmbed(title=f"**PRAWO JAZDY WOLNY TERMIN**",
-                                                     url="https://info-car.pl/new/prawo-jazdy/sprawdz-wolny-termin/wybor-terminu",
-                                                     color='7ef803')
-                                embed.set_footer(text='by makari')
-                                embed.set_timestamp()
-                                embed.add_embed_field(name="", value=f"Wolny termin dnia: **{exam_day}**  na godzine: **{exam_time}**",
-                                                      inline=False)
-                                webhook.add_embed(embed)
-                                webhook.execute()
+                                self.send_discord_webhook(exam_day, exam_time)
 
                                 last_date = datetime.strptime(exam_day + " " + exam_time, "%Y-%m-%d %H:%M:%S").timestamp()
                                 break
@@ -216,3 +226,22 @@ class Monitor:
             logging.error(f"Scrap dates error: {e}")
             retries += 1
             time.sleep(5)
+
+
+    def send_discord_webhook(self, exam_day, exam_time) -> None:
+        webhook = DiscordWebhook(
+            url=self.webhook_url,
+            content="@everyone")
+        webhook.execute()
+
+        webhook = DiscordWebhook(
+            url=self.webhook_url)
+        embed = DiscordEmbed(title=f"**PRAWO JAZDY WOLNY TERMIN**",
+                             url="https://info-car.pl/new/prawo-jazdy/sprawdz-wolny-termin/wybor-terminu",
+                             color='7ef803')
+        embed.set_footer(text='by makari')
+        embed.set_timestamp()
+        embed.add_embed_field(name="", value=f"Wolny termin dnia: **{exam_day}**  na godzine: **{exam_time}**",
+                              inline=False)
+        webhook.add_embed(embed)
+        webhook.execute()
